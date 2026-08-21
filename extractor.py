e7ce0b36c15b219b5946e97087437ebedcbcd7c7
Karthikeya-ai-max <karthikeya6367@gmail.com>
2026-08-21 10:35:07 +0530

    Initialize skill, technology, and language lists

--- extractor.py
+++ extractor.py
@@ -1,1 +1,11 @@
+text = input("Enter you resume: ")
+text = tex.lower()
+found_skill= []
+found_technology = []
+found_language = []
+skills = ["machine learning", "Artificial Intelligence", "Deep Learning", "LLP"]
+technologies = ["pytorch", "CNN", "tensorflow", "git", "github", "opencv"]
+languages = ["C++", "python", "java", "javascript", "sql"]
+
+
 
