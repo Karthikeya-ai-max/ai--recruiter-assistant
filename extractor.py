210581b28e81ffc46868e7c48e21cb69f924fa3f
Karthikeya-ai-max <karthikeya6367@gmail.com>
2026-08-21 13:05:25 +0530

    Refactor resume extractor for skills and technologies
    Updated variable names and improved skill extraction logic.

--- extractor.py
+++ extractor.py
@@ -1,11 +1,28 @@
-text = input("Enter you resume: ")
-text = tex.lower()
-found_skill= []
-found_technology = []
-found_language = []
-skills = ["machine learning", "Artificial Intelligence", "Deep Learning", "LLP"]
-technologies = ["pytorch", "CNN", "tensorflow", "git", "github", "opencv"]
-languages = ["C++", "python", "java", "javascript", "sql"]
+resume = input("Enter you resume: ")
+resume = resume.lower()
+Required_skill= []
+Required_technology = []
+Required_language = []
+skills = ["machine learning", "artificial intelligence", "deep learning", "computer vision"]
+technologies = ["pytorch", "cnn", "tensorflow"]
+languages = ["C++", "python", "java", "javascript"]
+for skill in skills:
+  if skill in resume:
+    Required_skill.append(skill)
+for technology in technologies:
+  if technology in resume:
+    required_technology.append(technology)
+for language in languages:
+  if language in resume:
+    required_language.append(language)
+
+print(f"Skills: {required_skills}")
+print(f"technologies: {required_technologies}")
+print(f"languages: {required_languages}")
+
+
+  
+  
 
 
 
